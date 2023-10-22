f4079b6cd0c2d9266b1b933e401893317cd7e988
BERKYT <BerkyT115@yandex.ru>
2023-10-22 03:38:37 +0300

    Отредачил гит игнор

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import EndPoint
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route(EndPoint.TEST)
+def test():  # put application's code here
+    return 'TEST'
 
 
 if __name__ == '__main__':
